020b1963d6b50f51997efb55023c301342dd251d
myroslava <murosja80@gmail.com>
2025-09-04 19:09:16 +0300

    add-about_us

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,14 @@ import Hero from "../Hero/Hero.jsx";
 import Header from "../Header/Header.jsx";
 
 import "./App.css";
+import AboutUs from "../AboutUs/AboutUs.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <AboutUs />
     </>
   );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,26 @@
+import Container from "../Container/Container.jsx";
 import styles from "./Hero.module.css";
 
 function Hero() {
   return (
-    <section className={styles.hero}>
-      <div>
-        <h1 className={styles.title}>
-          Explore the High Seas with Our Luxury Yacht Rentals
-        </h1>
-        <p className={styles.text}>
-          Experience the freedom and luxury of yachting on the open seas. Choose
-          from our top-of-the-line yachts and embark on an unforgettable journey
-          surrounded by breathtaking views and state-of-the-art amenities.
-        </p>
-        <a href="#rent" className={styles.link}>
-          Yacht rental
-        </a>
-      </div>
-    </section>
+    <Container>
+      <section className={styles.hero}>
+        <div>
+          <h1 className={styles.title}>
+            Explore the High Seas with Our Luxury Yacht Rentals
+          </h1>
+          <p className={styles.text}>
+            Experience the freedom and luxury of yachting on the open seas.
+            Choose from our top-of-the-line yachts and embark on an
+            unforgettable journey surrounded by breathtaking views and
+            state-of-the-art amenities.
+          </p>
+          <a href="#rent" className={styles.link}>
+            Yacht rental
+          </a>
+        </div>
+      </section>
+    </Container>
   );
 }
 export default Hero;
