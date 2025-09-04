4f6e33f6542e90ae80a499aa3af5d886ce143c4c
myroslava <murosja80@gmail.com>
2025-09-04 17:13:11 +0300

    hero

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "../Hero/Hero.jsx";
+import Header from "../Header/Header.jsx";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import styles from "./Hero.module.css";
+
+function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div>
+        <h1 className={styles.title}>
+          Explore the High Seas with Our Luxury Yacht Rentals
+        </h1>
+        <p className={styles.text}>
+          Experience the freedom and luxury of yachting on the open seas. Choose
+          from our top-of-the-line yachts and embark on an unforgettable journey
+          surrounded by breathtaking views and state-of-the-art amenities.
+        </p>
+        <a href="#rent" className={styles.link}>
+          Yacht rental
+        </a>
+      </div>
+    </section>
+  );
+}
+export default Hero;
